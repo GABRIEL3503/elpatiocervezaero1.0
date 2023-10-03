3c75f18606202cadcf4ffa5a044b0a64a4218f6e
gabriel M <gabriel_jm_3@hotmail.com>
2023-10-02 23:31:17 -0300

    2.0

--- public/script.js
+++ public/script.js
@@ -93,64 +93,80 @@ document.addEventListener("DOMContentLoaded", function () {
           },
           body: JSON.stringify(result.value)
         })
-        .then(response => response.json())
-        .then(data => {
-          console.log(data);
-          if (data.auth) {
-            localStorage.setItem('jwt', data.token);  // 
-            console.log('Token almacenado:', localStorage.getItem('jwt'));  // Verificar si el token se almacenó
-            window.location.reload();  // Recargar la página
+          .then(response => response.json())
+          .then(data => {
+            console.log(data);
+            if (data.auth) {
+              localStorage.setItem('jwt', data.token);  // 
+              console.log('Token almacenado:', localStorage.getItem('jwt'));  // Verificar si el token se almacenó
+              window.location.reload();  // Recargar la página
 
-          } else {
-            console.log('Credenciales inválidas');
-          }
-        });
+            } else {
+              console.log('Credenciales inválidas');
+            }
+          });
       }
     });
   });
-  
-function loadMenuItems() {
-  return fetch('https://elpatio.onrender.com/api/menu') // Asegúrate de que la URL sea la correcta
-    .then(response => response.json())
-    .then(data => {
-      const container = document.querySelector('.container');
-      let currentType = null;
 
-      data.data.forEach(item => {
-        let menuSection = document.querySelector(`.menu-section[data-type="${item.tipo}"]`);
+  function loadMenuItems() {
+    return fetch('https://elpatio.onrender.com/api/menu') // Asegúrate de que la URL sea la correcta
+      .then(response => response.json())
+      .then(data => {
+        const container = document.querySelector('.container');
+        let currentType = null;
 
-        if (!menuSection) {
-          menuSection = document.createElement('div');
-          menuSection.className = 'menu-section';
-          menuSection.setAttribute('data-type', item.tipo);
+        data.data.forEach(item => {
+          let menuSection = document.querySelector(`.menu-section[data-type="${item.tipo}"]`);
 
-          if (currentType !== item.tipo) {
-            const sectionTitle = document.createElement('h2');
-            sectionTitle.className = 'section-title';
-            sectionTitle.textContent = item.tipo.toUpperCase();
-            menuSection.appendChild(sectionTitle);
-            currentType = item.tipo;
+          if (!menuSection) {
+            menuSection = document.createElement('div');
+            menuSection.className = 'menu-section';
+            menuSection.setAttribute('data-type', item.tipo);
+          
+            if (currentType !== item.tipo) {
+              // Crear el elemento de imagen para el icono
+              const icon = document.createElement('img');
+              icon.className = 'section-icon';
+              
+              // Generar el nombre del archivo del icono
+              const iconName = item.tipo.toLowerCase().replace(/ /g, '_');
+              icon.src = `./img/${iconName}.png`; // Asegúrate de que la ruta sea la correcta
+              icon.alt = `${item.tipo} icon`;
+          
+              // Crear el título de la sección
+              const sectionTitle = document.createElement('h2');
+              sectionTitle.className = 'section-title';
+              sectionTitle.textContent = item.tipo.toUpperCase();
+          
+              // Añadir el icono al lado del título de la sección
+              const titleWithIcon = document.createElement('div');
+              titleWithIcon.className = 'title-with-icon';
+              titleWithIcon.appendChild(sectionTitle);
+              titleWithIcon.appendChild(icon);
+          
+              menuSection.appendChild(titleWithIcon);
+              currentType = item.tipo;
+            }
           }
+          
 
-          container.appendChild(menuSection);
-        }
+          const newItem = createMenuItem(item);
+          menuSection.appendChild(newItem);
+        });
+        applyOddItemClass();
+        checkAuthentication();
 
-        const newItem = createMenuItem(item);
-        menuSection.appendChild(newItem);
       });
-      applyOddItemClass();
-      checkAuthentication();
-
-    });
-    
-   
-}
 
 
-function createMenuItem(item) {
-  const newItem = document.createElement('div');
-  newItem.className = 'menu-item';
-  newItem.innerHTML = `
+  }
+
+
+  function createMenuItem(item) {
+    const newItem = document.createElement('div');
+    newItem.className = 'menu-item';
+    newItem.innerHTML = `
     <div class="item-header">
       <h3 class="item-title">${item.nombre}</h3>
       <span class="item-price">$${item.precio}</span>
@@ -158,104 +174,104 @@ function createMenuItem(item) {
     <p class="item-description">${item.descripcion}</p>
     <button class="edit-button auth-required">Editar</button>
   `;
-  newItem.dataset.id = item.id;
-  return newItem;
-}
-function applyOddItemClass() {
-  // Obtener todas las secciones del menú
-  const menuSections = document.querySelectorAll('.menu-section');
+    newItem.dataset.id = item.id;
+    return newItem;
+  }
+  function applyOddItemClass() {
+    // Obtener todas las secciones del menú
+    const menuSections = document.querySelectorAll('.menu-section');
 
-  // Iterar a través de cada sección y aplicar la clase 'odd-item' a las impares
-  menuSections.forEach((section, index) => {
-    if (index % 2 !== 0) {
-      section.classList.add('odd-item');
-    } else {
-      section.classList.remove('odd-item'); // Asegurarse de que las secciones pares no tengan la clase
-    }
-  });
-}
-
-
-document.body.addEventListener('click', function (event) {
-  if (event.target.classList.contains('edit-button')) {
-    const itemElement = event.target.closest('.menu-item');
-    const itemTitle = itemElement.querySelector('.item-title').textContent;
-    const itemPrice = itemElement.querySelector('.item-price').textContent.substring(1); // Eliminar el símbolo de dólar
-    const itemDescription = itemElement.querySelector('.item-description').textContent;
-    const itemType = event.target.closest('.menu-section').getAttribute('data-type');
-
-    Swal.fire({
-      title: 'Editar elemento',
-      html:
-        '<input id="swal-input1" class="swal2-input" placeholder="Nombre" value="' + itemTitle + '">' +
-        '<input id="swal-input2" class="swal2-input" placeholder="Precio" value="' + itemPrice + '">' +
-        '<input id="swal-input4" class="swal2-input" placeholder="Descripción" value="' + itemDescription + '">' +
-        '<select id="swal-input3" class="swal2-input">' +
-        '<option value="ENTRADAS" ' + (itemType === 'ENTRADAS' ? 'selected' : '') + '>ENTRADAS</option>' +
-        '<option value="PARA COMPARTIR" ' + (itemType === 'PARA COMPARTIR' ? 'selected' : '') + '>PARA COMPARTIR</option>' +
-        '<option value="SANDWICHES" ' + (itemType === 'SANDWICHES' ? 'selected' : '') + '>SANDWICHES</option>' +
-        '<option value="BEBIDAS" ' + (itemType === 'BEBIDAS' ? 'selected' : '') + '>BEBIDAS</option>' +
-        '<option value="CERVEZAS" ' + (itemType === 'CERVEZAS' ? 'selected' : '') + '>CERVEZAS</option>' +
-        '<option value="GIN" ' + (itemType === 'GIN' ? 'selected' : '') + '>GIN</option>' +
-        '</select>',
-      showCancelButton: true,
-      confirmButtonText: 'Actualizar',
-      cancelButtonText: 'Eliminar'
-    }).then((result) => {
-      const updatedData = {
-        nombre: document.getElementById('swal-input1').value,
-        precio: document.getElementById('swal-input2').value,
-        descripcion: document.getElementById('swal-input4').value,
-        tipo: document.getElementById('swal-input3').value
-      };
-
-
-      if (result.isConfirmed) {
-        console.log("ID del elemento a editar:", itemElement.dataset.id);
-        fetch(`/api/menu/${itemElement.dataset.id}`, {
-          method: 'PUT',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify(updatedData)
-        }).then(response => response.json())
-          .then(data => {
-            if (data.changes > 0) {
-              // El elemento se actualizó correctamente
-              console.log('Elemento actualizado');
-              // Actualizar el elemento en el frontend
-              itemElement.querySelector('.item-title').textContent = updatedData.nombre;
-              itemElement.querySelector('.item-price').textContent = `$${updatedData.precio}`;
-              itemElement.querySelector('.item-description').textContent = updatedData.descripcion;
-            
-              const oldMenuSection = event.target.closest('.menu-section');
-              const newMenuSection = document.querySelector(`.menu-section[data-type="${updatedData.tipo}"]`);
-              if (oldMenuSection !== newMenuSection) {
-                oldMenuSection.removeChild(itemElement);
-                newMenuSection.appendChild(itemElement);
-              }
-            }
-          });
-
-      } else if (result.dismiss === Swal.DismissReason.cancel) {
-        // Eliminar el elemento
-        fetch(`/api/menu/${itemElement.dataset.id}`, {
-          method: 'DELETE'
-        }).then(response => response.json())
-          .then(data => {
-            if (data.deleted > 0) {
-              // El elemento se eliminó correctamente
-              console.log('Elemento eliminado');
-              itemElement.remove();
-
-            }
-          });
+    // Iterar a través de cada sección y aplicar la clase 'odd-item' a las impares
+    menuSections.forEach((section, index) => {
+      if (index % 2 !== 0) {
+        section.classList.add('odd-item');
+      } else {
+        section.classList.remove('odd-item'); // Asegurarse de que las secciones pares no tengan la clase
       }
     });
   }
-});
 
-  
+
+  document.body.addEventListener('click', function (event) {
+    if (event.target.classList.contains('edit-button')) {
+      const itemElement = event.target.closest('.menu-item');
+      const itemTitle = itemElement.querySelector('.item-title').textContent;
+      const itemPrice = itemElement.querySelector('.item-price').textContent.substring(1); // Eliminar el símbolo de dólar
+      const itemDescription = itemElement.querySelector('.item-description').textContent;
+      const itemType = event.target.closest('.menu-section').getAttribute('data-type');
+
+      Swal.fire({
+        title: 'Editar elemento',
+        html:
+          '<input id="swal-input1" class="swal2-input" placeholder="Nombre" value="' + itemTitle + '">' +
+          '<input id="swal-input2" class="swal2-input" placeholder="Precio" value="' + itemPrice + '">' +
+          '<input id="swal-input4" class="swal2-input" placeholder="Descripción" value="' + itemDescription + '">' +
+          '<select id="swal-input3" class="swal2-input">' +
+          '<option value="ENTRADAS" ' + (itemType === 'ENTRADAS' ? 'selected' : '') + '>ENTRADAS</option>' +
+          '<option value="PARA COMPARTIR" ' + (itemType === 'PARA COMPARTIR' ? 'selected' : '') + '>PARA COMPARTIR</option>' +
+          '<option value="SANDWICHES" ' + (itemType === 'SANDWICHES' ? 'selected' : '') + '>SANDWICHES</option>' +
+          '<option value="BEBIDAS" ' + (itemType === 'BEBIDAS' ? 'selected' : '') + '>BEBIDAS</option>' +
+          '<option value="CERVEZAS" ' + (itemType === 'CERVEZAS' ? 'selected' : '') + '>CERVEZAS</option>' +
+          '<option value="GIN" ' + (itemType === 'GIN' ? 'selected' : '') + '>GIN</option>' +
+          '</select>',
+        showCancelButton: true,
+        confirmButtonText: 'Actualizar',
+        cancelButtonText: 'Eliminar'
+      }).then((result) => {
+        const updatedData = {
+          nombre: document.getElementById('swal-input1').value,
+          precio: document.getElementById('swal-input2').value,
+          descripcion: document.getElementById('swal-input4').value,
+          tipo: document.getElementById('swal-input3').value
+        };
+
+
+        if (result.isConfirmed) {
+          console.log("ID del elemento a editar:", itemElement.dataset.id);
+          fetch(`/api/menu/${itemElement.dataset.id}`, {
+            method: 'PUT',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(updatedData)
+          }).then(response => response.json())
+            .then(data => {
+              if (data.changes > 0) {
+                // El elemento se actualizó correctamente
+                console.log('Elemento actualizado');
+                // Actualizar el elemento en el frontend
+                itemElement.querySelector('.item-title').textContent = updatedData.nombre;
+                itemElement.querySelector('.item-price').textContent = `$${updatedData.precio}`;
+                itemElement.querySelector('.item-description').textContent = updatedData.descripcion;
+
+                const oldMenuSection = event.target.closest('.menu-section');
+                const newMenuSection = document.querySelector(`.menu-section[data-type="${updatedData.tipo}"]`);
+                if (oldMenuSection !== newMenuSection) {
+                  oldMenuSection.removeChild(itemElement);
+                  newMenuSection.appendChild(itemElement);
+                }
+              }
+            });
+
+        } else if (result.dismiss === Swal.DismissReason.cancel) {
+          // Eliminar el elemento
+          fetch(`/api/menu/${itemElement.dataset.id}`, {
+            method: 'DELETE'
+          }).then(response => response.json())
+            .then(data => {
+              if (data.deleted > 0) {
+                // El elemento se eliminó correctamente
+                console.log('Elemento eliminado');
+                itemElement.remove();
+
+              }
+            });
+        }
+      });
+    }
+  });
+
+
   // Cargar los elementos del menú
   loadMenuItems();
 
@@ -274,7 +290,7 @@ document.body.addEventListener('click', function (event) {
         '<option value="BEBIDAS">BEBIDAS</option>' +
         '<option value="CERVEZAS" >CERVEZAS</option>' +
         '<option value="GIN">GIN</option>' +
-      
+
         '</select>',
       focusConfirm: false,
       preConfirm: () => {
